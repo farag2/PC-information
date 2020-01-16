c30a3f0b31c3f24936fee157decd1f5f2a08fcc3
farag2 <farag2@users.noreply.github.com>
2020-01-16 12:50:57 +0300

    Update PC.ps1

--- PC.ps1
+++ PC.ps1
@@ -1,134 +1,96 @@
-#region User
-Write-Output User
-$PCName = @{
-	Name = "Computer name"
+#Requires -RunAsAdministrator
+
+Clear-Host
+
+#region BIOS
+Write-Output "`nBIOS"
+$Version = @{
+	Name = "Version"
 	Expression = {$_.Name}
 }
-$Domain = @{
-	Name = "Domain"
-	Expression = {$_.Domain}
-}
-$UserName = @{
-	Name = "User Name"
-	Expression = {$_.UserName}
-}
-(Get-CimInstance –ClassName CIM_ComputerSystem | Select-Object -Property $PCName, $Domain, $UserName | Format-Table | Out-String).Trim()
+(Get-CimInstance -ClassName CIM_BIOSElement | Select-Object -Property Manufacturer, $Version | Format-Table | Out-String).Trim()
+#endregion BIOS
 
-Write-Output "`nLocal Users"
-(Get-LocalUser | Out-String).Trim()
+#region Motherboard
+Write-Output "`nMotherboard"
+(Get-CimInstance -ClassName Win32_BaseBoard | Select-Object -Property Manufacturer, Product | Format-Table | Out-String).Trim()
+#endregion Motherboard
 
-Write-Output "`nGroup Membership"
-Get-ADPrincipalGroupMembership $env:USERNAME | Select-Object -Property Name
-#endregion User
+#region CPU
+Write-Output "`nCPU"
+$Cores = @{
+	Name = "Cores"
+	Expression = {$_.NumberOfCores}
+}
+$L3CacheSize = @{
+	Name = "L3, MB"
+	Expression = {$_.L3CacheSize / 1024}
+}
+$Threads = @{
+	Name = "Threads"
+	Expression = {$_.NumberOfLogicalProcessors}
+}
+(Get-CimInstance -ClassName CIM_Processor | Select-Object -Property Name, $Cores, $L3CacheSize, $Threads | Format-Table | Out-String).Trim()
+#endregion CPU
 
-#region Operating System
-Write-Output "`nOperating System"
-$ProductName = @{
-	Name = "Product Name"
-	Expression = {$_.Caption}
+#region RAM
+Write-Output "`nRAM"
+$Speed = @{
+	Name = "Speed, MHz"
+	Expression = {$_.Configuredclockspeed}
 }
-$InstallDate = @{
-	Name = "Install Date"
-	Expression=  {$_.InstallDate.Tostring().Split("")[0]}
+$Capacity = @{
+	Name = "Capacity, GB"
+	Expression = {$_.Capacity / 1GB}
 }
-$Arch = @{
-	Name = "Architecture"
-	Expression = {$_.OSArchitecture}
-}
-$a = Get-CimInstance -ClassName CIM_OperatingSystem | Select-Object -Property $ProductName, $InstallDate, $Arch
-$Build = @{
-	Name = "Build"
-	Expression = {"$($_.CurrentMajorVersionNumber).$($_.CurrentMinorVersionNumber).$($_.CurrentBuild).$($_.UBR)"}
-}
-$b = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows nt\CurrentVersion" | Select-Object -Property $Build
-([PSCustomObject] @{
-	"Product Name" = $a."Product Name"
-	Build = $b.Build
-	"Install Date" = $a."Install Date"
-	Architecture = $a.Architecture
-} | Out-String).Trim()
-#endregion Operating System
+(Get-CimInstance -ClassName CIM_PhysicalMemory | Select-Object -Property Manufacturer, PartNumber, $Speed, $Capacity | Format-Table | Out-String).Trim()
+#endregion RAM
 
-#region Registered apps
-Write-Output "`nRegistered apps"
-(Get-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*,HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName | Sort-Object
-#endregion Registered apps
-
-#region Updates
-Write-Output "`nInstalled updates supplied by CBS"
-$HotFixID = @{
-	Name = "KB ID"
-	Expression = {$_.HotFixID}
+#region Physical disks
+Write-Output "`nPhysical disks"
+$Model = @{
+	Name = "Model"
+	Expression = {$_.FriendlyName}
 }
-$InstalledOn = @{
-	Name = "Installed on"
-	Expression = {$_.InstalledOn.Tostring().Split("")[0]}
+$MediaType = @{
+	Name = "Drive type"
+	Expression = {$_.MediaType}
 }
-(Get-HotFix | Select-Object -Property $HotFixID, $InstalledOn -Unique | Format-Table | Out-String).Trim()
-
-Write-Output "`nInstalled updates supplied by MSI/WU"
-$Session = New-Object -ComObject "Microsoft.Update.Session"
-$Searcher = $Session.CreateUpdateSearcher()
-$historyCount = $Searcher.GetTotalHistoryCount()
-$KB = @{
-	Name = "KB ID"
-	Expression = {[regex]::Match($_.Title,"(KB[0-9]{6,7})").Value}
+$Size = @{
+	Name = "Size, GB"
+	Expression = {[math]::round($_.Size/1GB, 2)}
 }
-$Date = @{
-	Name = "Installed on"
-	Expression = {$_.Date.Tostring().Split("")[0]}
+$BusType = @{
+	Name = "Bus type"
+	Expression = {$_.BusType}
 }
-($Searcher.QueryHistory(0, $historyCount) | Where-Object -FilterScript {$_.Title -like "*KB*" -and $_.ResultCode -eq 2} | Select-Object $KB, $Date | Format-Table | Out-String).Trim()
-#endregion Updates
+(Get-PhysicalDisk | Select-Object -Property $Model, $MediaType, $BusType, $Size | Format-Table | Out-String).Trim()
+#endregion Physical disks
 
-#region Mapped disks
-Write-Output "`nMapped disks"
-(Get-SmbMapping | Select-Object -Property LocalPath, RemotePath | Format-Table | Out-String).Trim()
-#endregion Mapped disks
-
-#region Network
-Write-Output "`nDefault IP gateway"
-(Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration).DefaultIPGateway
-#endregion Network
-
-#region Windows Defender threats
-Write-Output "`nWindows Defender threats"
-enum ThreatStatusID
+#region Video сontrollers
+# Integrated graphics
+IF ((Get-CimInstance -ClassName CIM_VideoController | Where-Object -FilterScript {$_.AdapterDACType -eq "Internal"}))
 {
-	Unknown = 0
-	Detected = 1
-	Cleaned = 2
-	Quarantined = 3
-	Removed = 4
-	Allowed = 5
-	Blocked = 6
-	QuarantineFailed = 102
-	RemoveFailed = 103
-	AllowFailed = 104
-	Abondoned = 105
-	BlockedFailed = 107
+	$Caption = @{
+		Name = "Model"
+		Expression = {$_.Caption}
+	}
+	$VRAM = @{
+		Name = "VRAM, GB"
+		Expression = {[math]::round($_.AdapterRAM/1GB)}
+	}
+	Get-CimInstance -ClassName CIM_VideoController | Where-Object -FilterScript {$_.AdapterDACType -eq "Internal"} | Select-Object -Property $Caption, $VRAM
 }
-(Get-MpThreatDetection | ForEach-Object -Process {
-	[PSCustomObject] @{
-		"Detected Threats Paths" = $_.Resources
-		"ThreatID" = $_.ThreatID
-		"Status" = [System.Enum]::GetName([ThreatStatusID],$_.ThreatStatusID)
-		"Detection Time" = $_.InitialDetectionTime
+# Dedicated graphics
+IF ((Get-CimInstance -ClassName CIM_VideoController | Where-Object -FilterScript {$_.AdapterDACType -ne "Internal"}))
+{
+	$qwMemorySize = (Get-ItemProperty -Path "HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0*" -Name HardwareInformation.qwMemorySize -ErrorAction SilentlyContinue)."HardwareInformation.qwMemorySize"
+	$VRAM = [math]::round($qwMemorySize/1GB)
+	Get-CimInstance -ClassName CIM_VideoController | Where-Object -FilterScript {$_.AdapterDACType -ne "Internal"} | ForEach-Object -Process {
+		[PSCustomObject] @{
+			Model = $_.Caption
+			"VRAM, GB" = $VRAM
+		}
 	}
-} | Sort-Object ThreatID -Unique | Format-Table -AutoSize -Wrap | Out-String).Trim()
-#endregion Windows Defender threats
-
-#region Windows Defender settings
-Write-Output "`nWindows Defender settings"
-(Get-MpPreference | ForEach-Object -Process {
-	[PSCustomObject] @{
-		"Excluded IDs" = $_.ThreatIDDefaultAction_Ids | Out-String
-		"Excluded Process" = $_.ExclusionProcess | Out-String
-		"Controlled Folder Access" = $_.EnableControlledFolderAccess | Out-String
-		"Controlled Folder Access Protected Folders" = $_.ControlledFolderAccessProtectedFolders | Out-String
-		"Controlled Folder Access Allowed Applications" = $_.ControlledFolderAccessAllowedApplications | Out-String
-		"Excluded Extensions" = $_.ExclusionExtension | Out-String
-		"Excluded Paths" = $_.ExclusionPath | Out-String
-	}
-} | Format-List | Out-String).Trim()
-#endregion Windows Defender settings
+}
+#endregion Video сontrollers
