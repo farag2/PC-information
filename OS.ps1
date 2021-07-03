f8d02ea6e5420a69d0a663b17b913aa83430b4a2
Dmitry Nefedov <nefedovds@ya.ru>
2021-07-03 22:11:47 +0300

    Update OS.ps1

--- OS.ps1
+++ OS.ps1
@@ -1,24 +1,31 @@
-﻿#region User
+#Requires -RunAsAdministrator
+
+Clear-Host
+
+#region User
 Write-Output User
 $PCName = @{
-	Name = "Computer name"
+	Name       = "Computer name"
 	Expression = {$_.Name}
 }
 $Domain = @{
-	Name = "Domain"
+	Name       = "Domain"
 	Expression = {$_.Domain}
 }
 $UserName = @{
-	Name = "User Name"
+	Name       = "User Name"
 	Expression = {$_.UserName}
 }
-(Get-CimInstance –ClassName CIM_ComputerSystem | Select-Object -Property $PCName, $Domain, $UserName | Format-Table | Out-String).Trim()
+(Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object -Property $PCName, $Domain, $UserName | Format-Table | Out-String).Trim()
 
 Write-Output "`nLocal Users"
 (Get-LocalUser | Out-String).Trim()
 
 Write-Output "`nGroup Membership"
-Get-ADPrincipalGroupMembership $env:USERNAME | Select-Object -Property Name
+if ((Get-CimInstance -ClassName CIM_ComputerSystem).PartOfDomain -eq $true)
+{
+	Get-ADPrincipalGroupMembership $env:USERNAME | Select-Object -Property Name
+}
 #endregion User
 
 #region Operating System
@@ -28,30 +35,32 @@ $ProductName = @{
 	Expression = {$_.Caption}
 }
 $InstallDate = @{
-	Name = "Install Date"
-	Expression=  {$_.InstallDate.Tostring().Split("")[0]}
+	Name       = "Install Date"
+	Expression = {$_.InstallDate.Tostring().Split("")[0]}
 }
 $Arch = @{
-	Name = "Architecture"
+	Name       = "Architecture"
 	Expression = {$_.OSArchitecture}
 }
-$a = Get-CimInstance -ClassName CIM_OperatingSystem | Select-Object -Property $ProductName, $InstallDate, $Arch
+$OperatingSystem = Get-CimInstance -ClassName CIM_OperatingSystem | Select-Object -Property $ProductName, $InstallDate, $Arch
+
 $Build = @{
-	Name = "Build"
+	Name       = "Build"
 	Expression = {"$($_.CurrentMajorVersionNumber).$($_.CurrentMinorVersionNumber).$($_.CurrentBuild).$($_.UBR)"}
 }
-$b = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows nt\CurrentVersion" | Select-Object -Property $Build
+$CurrentVersion = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows nt\CurrentVersion" | Select-Object -Property $Build
+
 ([PSCustomObject] @{
-	"Product Name" = $a."Product Name"
-	Build = $b.Build
-	"Install Date" = $a."Install Date"
-	Architecture = $a.Architecture
+	"Product Name" = $OperatingSystem."Product Name"
+	"Install Date" = $OperatingSystem."Install Date"
+	Build = $CurrentVersion.Build
+	Architecture = $OperatingSystem.Architecture
 } | Out-String).Trim()
 #endregion Operating System
 
 #region Registered apps
 Write-Output "`nRegistered apps"
-(Get-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*,HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName | Sort-Object
+(Get-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName | Sort-Object
 #endregion Registered apps
 
 #region Updates
@@ -61,7 +70,7 @@ $HotFixID = @{
 	Expression = {$_.HotFixID}
 }
 $InstalledOn = @{
-	Name = "Installed on"
+	Name       = "Installed on"
 	Expression = {$_.InstalledOn.Tostring().Split("")[0]}
 }
 (Get-HotFix | Select-Object -Property $HotFixID, $InstalledOn -Unique | Format-Table | Out-String).Trim()
@@ -71,11 +80,11 @@ $Session = New-Object -ComObject "Microsoft.Update.Session"
 $Searcher = $Session.CreateUpdateSearcher()
 $historyCount = $Searcher.GetTotalHistoryCount()
 $KB = @{
-	Name = "KB ID"
+	Name       = "KB ID"
 	Expression = {[regex]::Match($_.Title,"(KB[0-9]{6,7})").Value}
 }
 $Date = @{
-	Name = "Installed on"
+	Name       = "Installed on"
 	Expression = {$_.Date.Tostring().Split("")[0]}
 }
 ($Searcher.QueryHistory(0, $historyCount) | Where-Object -FilterScript {$_.Title -like "*KB*" -and $_.ResultCode -eq 2} | Select-Object $KB, $Date | Format-Table | Out-String).Trim()
@@ -84,24 +93,24 @@ $Date = @{
 #region Logical drives
 Write-Output "`nLogical drives"
 $Name = @{
-	Name = "Name"
+	Name       = "Name"
 	Expression = {$_.DeviceID}
 }
 enum DriveType
 {
-	RemovableDrive	=	2
-	HardDrive	=	3
+	RemovableDrive = 2
+	HardDrive      = 3
 }
 $Type = @{
-	Name = "Drive Type"
+	Name       = "Drive Type"
 	Expression = {[System.Enum]::GetName([DriveType],$_.DriveType)}
 }
 $Size = @{
-	Name = "Size, GB"
+	Name       = "Size, GB"
 	Expression = {[math]::round($_.Size/1GB, 2)}
 }
 $FreeSpace = @{
-	Name = "FreeSpace, GB"
+	Name       = "FreeSpace, GB"
 	Expression = {[math]::round($_.FreeSpace/1GB, 2)}
 }
 (Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object -FilterScript {$_.DriveType -ne 4} | Select-Object -Property $Name, $Type, $Size, $FreeSpace | Format-Table | Out-String).Trim()
@@ -120,48 +129,49 @@ Get-CimInstance -ClassName CIM_Printer | Select-Object -Property Name, Default, 
 #region Network
 Write-Output "`nDefault IP gateway"
 (Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration).DefaultIPGateway
+
 Write-Output "`nDNS"
-(Get-DnsClientServerAddress).ServerAddresses
+(Get-DnsClientServerAddress -Family IPv4).ServerAddresses
 #endregion Network
 
-#region Windows Defender threats
-Write-Output "`nWindows Defender threats"
+#region Microsoft Defender threats
+Write-Output "`nMicrosoft Defender threats"
 enum ThreatStatusID
 {
-	Unknown = 0
-	Detected = 1
-	Cleaned = 2
-	Quarantined = 3
-	Removed = 4
-	Allowed = 5
-	Blocked = 6
+	Unknown          = 0
+	Detected         = 1
+	Cleaned          = 2
+	Quarantined      = 3
+	Removed          = 4
+	Allowed          = 5
+	Blocked          = 6
 	QuarantineFailed = 102
-	RemoveFailed = 103
-	AllowFailed = 104
-	Abondoned = 105
-	BlockedFailed = 107
+	RemoveFailed     = 103
+	AllowFailed      = 104
+	Abondoned        = 105
+	BlockedFailed    = 107
 }
 (Get-MpThreatDetection | ForEach-Object -Process {
 	[PSCustomObject] @{
 		"Detected Threats Paths" = $_.Resources
-		"ThreatID" = $_.ThreatID
-		"Status" = [System.Enum]::GetName([ThreatStatusID],$_.ThreatStatusID)
-		"Detection Time" = $_.InitialDetectionTime
+		"ThreatID"             = $_.ThreatID
+		"Status"               = [System.Enum]::GetName([ThreatStatusID],$_.ThreatStatusID)
+		"Detection Time"       = $_.InitialDetectionTime
 	}
 } | Sort-Object ThreatID -Unique | Format-Table -AutoSize -Wrap | Out-String).Trim()
-#endregion Windows Defender threats
+#endregion Microsoft Defender threats
 
-#region Windows Defender settings
-Write-Output "`nWindows Defender settings"
+#region Microsoft Defender settings
+Write-Output "`nMicrosoft Defender settings"
 (Get-MpPreference | ForEach-Object -Process {
 	[PSCustomObject] @{
-		"Excluded IDs" = $_.ThreatIDDefaultAction_Ids | Out-String
-		"Excluded Process" = $_.ExclusionProcess | Out-String
-		"Controlled Folder Access" = $_.EnableControlledFolderAccess | Out-String
-		"Controlled Folder Access Protected Folders" = $_.ControlledFolderAccessProtectedFolders | Out-String
+		"Excluded IDs"                                  = $_.ThreatIDDefaultAction_Ids | Out-String
+		"Excluded Process"                              = $_.ExclusionProcess | Out-String
+		"Controlled Folder Access"                      = $_.EnableControlledFolderAccess | Out-String
+		"Controlled Folder Access Protected Folders"    = $_.ControlledFolderAccessProtectedFolders | Out-String
 		"Controlled Folder Access Allowed Applications" = $_.ControlledFolderAccessAllowedApplications | Out-String
-		"Excluded Extensions" = $_.ExclusionExtension | Out-String
-		"Excluded Paths" = $_.ExclusionPath | Out-String
+		"Excluded Extensions"                           = $_.ExclusionExtension | Out-String
+		"Excluded Paths"                                = $_.ExclusionPath | Out-String
 	}
 } | Format-List | Out-String).Trim()
 #endregion Windows Defender settings
